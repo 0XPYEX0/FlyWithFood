80f35fd58ee2b80d10b827cf6f64e118fd8a75ab
0XPYEX0 <15880561966@163.com>
2022-02-01 17:15:04 +0800

    修改版本号

--- src/main/java/me/xpyex/plugin/flywithfood/sponge/FlyWithFood.java
+++ src/main/java/me/xpyex/plugin/flywithfood/sponge/FlyWithFood.java
@@ -26,7 +26,7 @@ import org.spongepowered.api.service.economy.EconomyService;
         authors = {
                 "XPYEX"
         },
-        version = "1.2.6"
+        version = "1.3.0"
 )
 public class FlyWithFood {
     public static FlyWithFood INSTANCE;
