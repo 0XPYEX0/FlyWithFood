26279d364717838685029aafc7942bdb694214cd
0XPYEX0 <15880561966@163.com>
2022-03-23 18:47:28 +0800

    修复在Essentials管控经济的前提下扣款无效的问题

--- src/main/java/me/xpyex/plugin/flywithfood/bukkit/implementations/energys/BukkitMoney.java
+++ src/main/java/me/xpyex/plugin/flywithfood/bukkit/implementations/energys/BukkitMoney.java
@@ -4,7 +4,7 @@ import me.xpyex.plugin.flywithfood.bukkit.FlyWithFood;
 import me.xpyex.plugin.flywithfood.common.implementations.FWFUser;
 import me.xpyex.plugin.flywithfood.common.implementations.flyenergy.EnergyManager;
 import me.xpyex.plugin.flywithfood.common.implementations.flyenergy.energys.MoneyEnergy;
-import org.bukkit.Bukkit;
+import net.milkbowl.vault.economy.EconomyResponse;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
@@ -16,11 +16,17 @@ public class BukkitMoney implements MoneyEnergy {
 
     @Override
     public void cost(@NotNull FWFUser user, @NotNull Number value) {
+        if (value.doubleValue() == 0) {  //+-0没有变化
+            return;
+        }
         if (FlyWithFood.ECON != null) {
             Player target = (Player) user.getPlayer();
-            Bukkit.getScheduler().runTask(FlyWithFood.INSTANCE, () ->
-                    FlyWithFood.ECON.depositPlayer(target, -value.doubleValue())  //才发现这玩意叫存款，，我还以为是设置金额
-            );  //看看同步执行能不能修好那个奇怪的Bug
+            EconomyResponse result = value.doubleValue() > 0 ?
+                    FlyWithFood.ECON.withdrawPlayer(target, value.doubleValue()) :  //Ess不允许存入负数的钱款
+                    FlyWithFood.ECON.depositPlayer(target, -value.doubleValue());  //存款
+            if (!result.transactionSuccess()) {
+                FlyWithFood.LOGGER.severe("处理玩家 " + target.getName() + " 的游戏币时出现错误: " + result.errorMessage);
+            }
         }
     }
 
