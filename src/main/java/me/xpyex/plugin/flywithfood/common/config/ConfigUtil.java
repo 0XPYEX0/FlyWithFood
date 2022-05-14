802d4936ed424802ee2acc6682b351035c2a9528
0XPYEX0 <15880561966@163.com>
2022-05-14 17:48:45 +0800

    修复低版本(1.8-)在保护玩家免于摔伤时的错误

--- src/main/java/me/xpyex/plugin/flywithfood/common/config/ConfigUtil.java
+++ src/main/java/me/xpyex/plugin/flywithfood/common/config/ConfigUtil.java
@@ -15,7 +15,7 @@ public class ConfigUtil {
             .setPrettyPrinting()
             .disableHtmlEscaping()
             .create();
-    public static Boolean isOldGson;  //基元类型不为空，所以用非基元类型
+    public static boolean isOldGson;  //基元类型不为空，所以用非基元类型
 
     static {
         try {
