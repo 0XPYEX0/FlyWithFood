44aaaa7db195ec0293e79c1cbbc899c2e36922f6
XPYEX <XPYEX0@163.com>
2022-08-15 17:23:10 +0800

    2.x重写中

--- src/main/java/me/xpyex/plugin/flywithfood/bukkit/reflections/NMSAll.java
+++
@@ -1,8 +0,0 @@
-package me.xpyex.plugin.flywithfood.bukkit.reflections;
-
-import org.bukkit.Bukkit;
-
-public class NMSAll {
-    public static final String NMS_VERSION = Bukkit.getServer().getClass().getPackage().getName().split("\\.")[3];
-    public static boolean shouldUseNMSAction = false;
-}
