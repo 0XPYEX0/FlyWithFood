8c3b345120b827d55032d0503103e26c8f5e65df
XPYEX <XPYEX0@163.com>
2023-09-06 15:56:52 +0800

    新增提示

--- bukkit/src/main/java/me/xpyex/plugin/flywithfood/bukkit/extra/ResidenceHook.java
+++ bukkit/src/main/java/me/xpyex/plugin/flywithfood/bukkit/extra/ResidenceHook.java
@@ -1,6 +1,7 @@
 package me.xpyex.plugin.flywithfood.bukkit.extra;
 
 import java.lang.reflect.Method;
+import me.xpyex.plugin.flywithfood.common.FlyWithFood;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
@@ -11,7 +12,10 @@ public class ResidenceHook {
         try {
             RES_MANAGER = Class.forName("com.bekvon.bukkit.residence.api.ResidenceApi").getMethod("getResidenceManager").invoke(null);
             GET_RES_METHOD = RES_MANAGER.getClass().getMethod("getByLoc", Location.class);
-        } catch (ReflectiveOperationException ignored) {}
+            FlyWithFood.getLogger().info("检测到Residence，已挂钩");
+        } catch (ReflectiveOperationException ignored) {
+            FlyWithFood.getLogger().info("未检测到Residence，相关功能将不生效");
+        }
     }
 
     public static boolean isInResidence(Player p) {
